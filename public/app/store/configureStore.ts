d33b41767e9ca46a50643a85fc581ebd9e8e8647
edum18 <edu.miguel95@hotmail.com>
2019-01-16 12:01:00 +0000

    tirei o logger para tirar os logs "update location"

--- public/app/store/configureStore.ts
+++ public/app/store/configureStore.ts
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+//import { createLogger } from 'redux-logger';
 import sharedReducers from 'app/core/reducers';
 import alertingReducers from 'app/features/alerting/state/reducers';
 import teamsReducers from 'app/features/teams/state/reducers';
@@ -28,10 +28,11 @@ export let store;
 export function configureStore() {
   const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-  if (process.env.NODE_ENV !== 'production') {
-    // DEV builds we had the logger middleware
-    store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(thunk, createLogger())));
-  } else {
-    store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(thunk)));
-  }
+  // console.log(process.env.NODE_ENV);
+  // if (process.env.NODE_ENV !== 'production') {
+  // DEV builds we had the logger middleware
+  // store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(thunk, createLogger())));
+  // } else {
+  store = createStore(rootReducer, {}, composeEnhancers(applyMiddleware(thunk)));
+  // }
 }
